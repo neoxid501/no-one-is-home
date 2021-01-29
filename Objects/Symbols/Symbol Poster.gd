fb6e3b3cb3abf8768c6a81e02dce11768ea9cf50
neoxid501 <carlo97dm@gmail.com>
2021-01-29 15:32:14 -0800

    Added Beacons on Posters

--- Objects/Symbols/Symbol Poster.gd
+++ Objects/Symbols/Symbol Poster.gd
@@ -1,8 +1,10 @@
 extends Spatial
 
 #child nodes
-onready var symbol = $Symbol
-onready var label = $OQ_UILabel
+onready var beacon = $Beacon
+onready var poster = $Poster
+onready var symbol = $Poster/Symbol
+onready var label = $Poster/OQ_UILabel
 
 func _ready():
 	pass
@@ -14,3 +16,15 @@ func set_values(s, n, p=null):
 	label.set_label_text(str(n))
 	if p != null:
 		global_transform.origin = p
+
+
+func _on_Area_body_entered(body):
+	if body.is_in_group("Player"):
+		beacon.hide()
+		poster.show()
+
+
+func _on_Area_body_exited(body):
+	if body.is_in_group("Player"):
+		beacon.show()
+		poster.hide()
